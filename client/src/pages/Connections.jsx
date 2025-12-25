b2ae53613fc928da4e43af275e46927d7c7f3a0d
Vansh Mehta <vanshmehta@gmail.com>
2025-12-25 12:32:07 +0530

    solve the accept button issue

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from '@clerk/clerk-react';
 import { fetchConnections } from '../features/connections/connectionSlice';
 import api from '../api/axios.js'
+import { toast } from 'react-hot-toast'
 
 const Connections = () => {
     const { connections, pendingConnections, followers, following } = useSelector((state) => state.connection)
