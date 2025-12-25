118ccf00a77e8142cfb9a2ea40be09c4ab0c6816
Vansh Mehta <vanshmehta@gmail.com>
2025-12-25 12:58:09 +0530

    solving errors

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -7,6 +7,6 @@ export const store = configureStore({
     reducer: {
         user: userReducer,
         connection: connectionReducer,
-        message: messageReducer,
+        messages: messageReducer,
     }
 })
